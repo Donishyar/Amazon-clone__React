925c6abe7ace777bd150758ed31d5ae3abfb4ba0
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-03-28 17:09:04 +0430

    working on home items

--- src/Product.js
+++ src/Product.js
@@ -4,9 +4,9 @@ import "./Product.css";
 function Product() {
   return (
     <div className="product">
-      <div className="product__info">
-        <h1 className="product__title">Gaming accessories</h1>
-      </div>
+      <h1 className="product__title">Gaming accessories</h1>
+      <img src="https://i.ibb.co/XFy1z3m/Capture.png" alt="product-image" />
+      <a href="#">See more</a>
     </div>
   );
 }
